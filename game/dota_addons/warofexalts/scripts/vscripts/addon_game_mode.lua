52a3dd483eb87f832e6c1e841ba07d977b3fd581
Adam Curtis <acurtis@spsu.edu>
2015-07-06 11:54:31 -0400

    Initial commit
    Mostly ModKit and Barebones stuff

---
+++ game/dota_addons/warofexalts/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,49 @@
+requires = {
+	'util',
+	'timers',
+	'physics',
+	'lib.statcollection',
+	'abilities',
+    'woe_unit_wrapper',
+	'warofexalts',
+}
+
+function Precache( context )
+	-- NOTE: IT IS RECOMMENDED TO USE A MINIMAL AMOUNT OF LUA PRECACHING, AND A MAXIMAL AMOUNT OF DATADRIVEN PRECACHING.
+	-- Precaching guide: https://moddota.com/forums/discussion/119/precache-fixing-and-avoiding-issues
+
+	print("[WAROFEXALTS] Performing pre-load precache")
+
+	-- Particles can be precached individually or by folder
+	-- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
+	--PrecacheResource("particle", "particles/econ/generic/generic_aoe_explosion_sphere_1/generic_aoe_explosion_sphere_1.vpcf", context)
+	--PrecacheResource("particle_folder", "particles/test_particle", context)
+
+	-- Models can also be precached by folder or individually
+	--PrecacheModel should generally used over PrecacheResource for individual models
+	--PrecacheResource("model_folder", "particles/heroes/antimage", context)
+	--PrecacheResource("model", "particles/heroes/viper/viper.vmdl", context)
+	--PrecacheModel("models/heroes/viper/viper.vmdl", context)
+
+	-- Sounds can precached here like anything else
+	PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_gyrocopter.vsndevts", context)
+
+	-- Entire items can be precached by name
+	-- Abilities can also be precached in this way despite the name
+	PrecacheItemByNameSync("example_ability", context)
+	PrecacheItemByNameSync("item_example_item", context)
+
+	-- Entire heroes (sound effects/voice/models/particles) can be precached with PrecacheUnitByNameSync
+	-- Custom units from npc_units_custom.txt can also have all of their abilities and precache{} blocks precached in this way
+	PrecacheUnitByNameSync("npc_dota_hero_ancient_apparition", context)
+end
+
+-- Create the game mode when we activate
+function Activate()
+	GameRules.WarOfExalts = WarOfExalts()
+	GameRules.WarOfExalts:InitWarOfExalts()
+end
+
+for i,v in ipairs(requires) do
+	require(v)	
+end
---
+++ game/dota_addons/warofexalts/scripts/vscripts/woe_unit_wrapper.lua
@@ -0,0 +1,73 @@
+--This function takes a dota NPC and adds custom WoE functionality to it
+function WoeUnitWrapper(unit)
+    keys = keys or {}
+    --Special flag we can use to identify a WoE unit
+    unit.isWoeUnit = true
+    --Initialize WoE instance variables
+    unit._woeKeys = {
+        spellHaste = 0,
+        magicResistBase = 0,
+        magicResistBonus = 0,
+        cdrBonus = 0 
+    }
+    
+    --Add WoE-specific methods to the unit
+    
+    --Get the total WoE MR rating (analogous to armor rating)
+    function unit:GetWoeMagicResist() 
+        return self._woeKeys.magicResistBase + self._woeKeys.magicResistBonus
+    end
+    
+    --Get only the base MR rating
+    function unit:GetWoeMagicResistBase()
+        return self._woeKeys.magicResistBase
+    end
+    
+    --Get only the bonus MR rating
+    function unit:GetWoeMagicResistBonus()
+        return self._woeKeys.magicResistBonus
+    end
+    
+    --Set the base MR rating of the unit
+    function unit:SetWoeMagicResistBase(v)
+        self._woeKeys.magicResistBase = v
+        self:_RecalculateMagicReduction()
+    end
+    
+    --Set the bonus MR rating of the unit
+    function unit:SetWoeMagicResistBonus(v)
+        self._woeKeys.magicResistBonus = v
+        self:_RecalculateMagicReduction()
+    end
+    
+    --Calculates % magic reduction from current MR rating and resets it via the dota API
+    function unit:_RecalculateMagicReduction()
+        local mr = self:GetWoeMagicResist()
+        self:SetBaseMagicalResistanceValue(0.06 * mr / (1 + 0.06 * mr))
+    end
+    
+    --Get spell haste rating
+    function unit:GetSpellHaste()
+        return self._woeKeys.spellHaste
+    end
+    
+    --Set spell haste rating
+    function unit:SetSpellHaste()
+        return self._woeKeys.spellHaste
+    end
+    
+    function unit:GetCdrBonus()
+        return self._woeKeys.cdrBonus
+    end
+    
+    function unit:SetCdrBonus(v)
+        self._woeKeys.cdrBonus = v
+    end
+    
+    if unit:IsHero() then
+        unit:AddNewModifier(nil, nil, "modifier_woe_attributes", {})
+    end
+    
+end
+        
+    
