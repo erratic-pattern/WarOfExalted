019ec54d612b9513788fc2697815eb1351094a1e
Adam Curtis <acurtis@spsu.edu>
2015-07-08 18:29:40 -0400

    fix up combine KV script

--- game/dota_addons/warofexalts/scripts/npc/combine_kv.sh
+++ game/dota_addons/warofexalts/scripts/npc/combine_kv.sh
@@ -6,13 +6,17 @@ combine_kv () {
     local outFile=$3
     if [ -d $inDir ]; then
         echo \"$rootName\"' {' > $outFile
-        find $inDir -type f | sed -i -e 's/^/\t/g' >> $outFile
+        sed -e 's/^/\t/g' $inDir/*.txt >> $outFile
         echo '}' >> $outFile
     else
         echo "Directory not found: $inDir"
     fi
 }
 
+combine_kv DOTAAbilities abilities npc_abilities_custom.txt
+combine_kv DOTAHeroes heroes npc_heroes_custom.txt
+combine_kv DOTAItems items npc_items_custom.txt
+combine_kv DOTAUnits units npc_units_custom.txt
 combine_kv WoeAbilities abilities/woe woe_abilities.txt
 combine_kv WoeItems items/woe woe_items.txt
 combine_kv WoeUnits units/woe woe_units.txt
