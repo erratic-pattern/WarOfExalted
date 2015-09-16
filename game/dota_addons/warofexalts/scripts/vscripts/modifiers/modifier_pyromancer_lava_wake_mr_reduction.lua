056c45c06a077c2ee026c404815d59b0364f2355
Adam Curtis <acurtis@spsu.edu>
2015-09-16 13:46:04 -0400

    uncomment EffectAttachType in lava wake debuff

--- game/dota_addons/warofexalts/scripts/vscripts/modifiers/modifier_pyromancer_lava_wake_mr_reduction.lua
+++ game/dota_addons/warofexalts/scripts/vscripts/modifiers/modifier_pyromancer_lava_wake_mr_reduction.lua
@@ -7,7 +7,7 @@ modifier_pyromancer_lava_wake_mr_reduction:Init({
     IsHidden = false,
     EffectName = "particles/units/heroes/hero_phoenix/phoenix_fire_spirit_burn_creep.vpcf",
     HeroEffectName = "particles/units/heroes/hero_phoenix/phoenix_fire_spirit_burn.vpcf",
-    --EffectAttachType = PATTACH_ABSORIGIN_FOLLOW
+    EffectAttachType = PATTACH_ABSORIGIN_FOLLOW
 })
 
 modifier_pyromancer_lava_wake_mr_reduction:WoeProperties({
