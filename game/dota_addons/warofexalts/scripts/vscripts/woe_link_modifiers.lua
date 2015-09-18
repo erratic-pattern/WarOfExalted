d58bb3e5711313d4189cfc790f36ff85a5a2372b
Adam Curtis <acurtis@spsu.edu>
2015-09-18 14:13:12 -0400

    comment out modifier_flameshaper_intensification
    also fixed a typo

--- game/dota_addons/warofexalts/scripts/vscripts/woe_link_modifiers.lua
+++ game/dota_addons/warofexalts/scripts/vscripts/woe_link_modifiers.lua
@@ -9,10 +9,10 @@ function WarOfExalts:LinkModifiers()
     --Core game modifiers
     LinkLuaModifier("modifier_client_side_init", "modifiers/modifier_client_side_init", LUA_MODIFIER_MOTION_NONE)
     LinkLuaModifier("modifier_woe_base", "modifiers/modifier_woe_base", LUA_MODIFIER_MOTION_NONE) -- Note: this needs to be linked before most other modifiers
-    LinkLuaModifier("modifier_woe_attributes", "modifiers/modifier_woe_attributes", LUA_MODIFIER_MOTION_NONE)
     LinkLuaModifier("modifier_woe_stamina_regenerator", "modifiers/modifier_woe_stamina_regenerator", LUA_MODIFIER_MOTION_NONE)
+    LinkLuaModifier("modifier_woe_attributes", "modifiers/modifier_woe_attributes", LUA_MODIFIER_MOTION_NONE)
     
     --Flameshaper
     LinkLuaModifier("modifier_flameshaper_lava_wake_mr_reduction", "modifiers/modifier_flameshaper_lava_wake_mr_reduction", LUA_MODIFIER_MOTION_NONE)
-    LinkLuaModifier("modifier_flameshaper_intensification", "modifiers/modifier_flameshaper_intensifcation", LUA_MODIFIER_MOTION_NONE)
+    --LinkLuaModifier("modifier_flameshaper_intensification", "modifiers/modifier_flameshaper_intensifcation", LUA_MODIFIER_MOTION_NONE)
 end
