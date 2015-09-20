3c5964f8501a408c75c47e67c01caf73e0e433df
Adam Curtis <acurtis@spsu.edu>
2015-09-20 12:49:44 -0400

    whitespace

--- game/dota_addons/warofexalts/scripts/vscripts/woe_link_modifiers.lua
+++ game/dota_addons/warofexalts/scripts/vscripts/woe_link_modifiers.lua
@@ -10,6 +10,5 @@ function WarOfExalts:LinkModifiers()
     LinkLuaModifier("modifier_client_side_init", "modifiers/modifier_client_side_init", LUA_MODIFIER_MOTION_NONE)
     LinkLuaModifier("modifier_base", "modifiers/modifier_base", LUA_MODIFIER_MOTION_NONE) -- Note: this needs to be linked before most other modifiers
     LinkLuaModifier("modifier_woe_stamina_regenerator", "modifiers/modifier_woe_stamina_regenerator", LUA_MODIFIER_MOTION_NONE)
-    --LinkLuaModifier("modifier_woe_attributes", "modifiers/modifier_woe_attributes", LUA_MODIFIER_MOTION_NONE)
-    
+    --LinkLuaModifier("modifier_woe_attributes", "modifiers/modifier_woe_attributes", LUA_MODIFIER_MOTION_NONE) 
 end
