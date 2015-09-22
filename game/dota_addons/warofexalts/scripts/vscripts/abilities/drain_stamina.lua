9221ed582d2f15ff9b135af238b3cb63d0e1a803
Adam Curtis <acurtis@spsu.edu>
2015-09-21 20:09:18 -0400

    remove debugging junk from drain_stamina

--- game/dota_addons/warofexalts/scripts/vscripts/abilities/drain_stamina.lua
+++ game/dota_addons/warofexalts/scripts/vscripts/abilities/drain_stamina.lua
@@ -2,10 +2,7 @@ drain_stamina = class({})
 
 
 function drain_stamina:OnToggle()
-    self.timer = nil
     if IsServer() then
-        local t = self:GetCaster()._propsCache.StaminaRegenBase
-        util.debugTable("cache", t)
         self.timer = Timers:CreateTimer(0, function()
             if self:GetToggleState() then
                 self:GetCaster():SpendStamina(10)
