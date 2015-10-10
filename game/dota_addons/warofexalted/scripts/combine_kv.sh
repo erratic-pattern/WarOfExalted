53513df55f739d02072dd32a8c3fde124d7dcfa9
Adam Curtis <acurtis@spsu.edu>
2015-10-10 12:09:22 -0400

    move combine_kv.sh script up a directory

--- game/dota_addons/warofexalted/scripts/combine_kv.sh
+++ game/dota_addons/warofexalted/scripts/combine_kv.sh
@@ -13,14 +13,8 @@ combine_kv () {
         echo "Directory not found: $inDir"
     fi
 }
-
+cd npc
 combine_kv DOTAAbilities abilities npc_abilities_custom.txt
 combine_kv DOTAHeroes heroes npc_heroes_custom.txt
 combine_kv DOTAItems items npc_items_custom.txt
 combine_kv DOTAUnits units npc_units_custom.txt
-
-##uncomment if we want to split WoE KVs into seperate files
-#combine_kv WoeAbilities abilities/woe woe_abilities.txt
-#combine_kv WoeItems items/woe woe_items.txt
-#combine_kv WoeUnits units/woe woe_units.txt
-#combine_kv WoeHeroes heroes/woe woe_heroes.txt
